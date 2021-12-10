dda1cda26b0784a481de9816bab1adca7b3691e0
Andra Coteanu <72747266+AndraCoteanu@users.noreply.github.com>
2021-12-10 23:24:59 +0200

    update GUI
    updated the GUI from the previous commit - cleaned the code

--- Table/backgammon.py
+++ Table/backgammon.py
@@ -1,19 +1,21 @@
-import random
 import webbrowser
 from tkinter import *
 from tkinter import font
 
+import game as game
+import utils
+
+window = Tk()
+window.title("Backgammon!!")
+window.configure(width=1200, height=800)
+window.resizable(False, False)
+
+icon = PhotoImage(file="assets/icon.png")
+window.iconphoto(False, icon)
+
 
 def main_menu():
-    window = Tk()
-    window.title("Backgammon!!")
-    window.configure(width=1200, height=700)
-    window.resizable(False, False)
-
-    icon = PhotoImage(file="assets/icon.png")
-    window.iconphoto(False, icon)
-
-    main_screen = Frame(window, width=1200, height=700)
+    main_screen = Frame(window, width=1200, height=800)
     main_screen.pack(side="top", expand=False, fill="both")
 
     background = PhotoImage(
@@ -21,25 +23,18 @@ def main_menu():
     label_background = Label(main_screen, image=background)
     label_background.place(x=-5, y=-2)
 
-    text_font = font.Font(size=20)
+    text_font = font.Font(size=15)
     btn_help = Button(main_screen, text="Instructiuni", bg="#c2fcdd", fg="#002e1d", font=text_font,
                       command=help_callback)
-    btn_help.place(x=530, y=260)
+    btn_help.place(x=530, y=340)
 
     btn_player = Button(main_screen, text="Player vs player", bg="#c2fcdd", fg="#002e1d", font=text_font,
                         command=lambda: player_callback(main_screen))
-    btn_player.place(x=500, y=340)
+    btn_player.place(x=505, y=400)
 
     btn_pc = Button(main_screen, text="Player vs PC", bg="#c2fcdd", fg="#002e1d", font=text_font,
                     command=lambda: pc_callback(main_screen))
-    btn_pc.place(x=520, y=420)
-
-    # SOUND_ON = True
-    # for some reason python stops working when trying to run playsound
-    # playsound.playsound("assets/music.mp3")
-    # SOUND = PhotoImage(file="assets/sound-on.png")
-    # BUTTON_SOUND = Button(window, text="", image=SOUND, bg="#c2fcdd", command=sound_callback)
-    # BUTTON_SOUND.place(x=10, y=10)
+    btn_pc.place(x=520, y=460)
 
     window.mainloop()
 
@@ -47,6 +42,7 @@ def main_menu():
 def clear_frame(frame):
     for widgets in frame.winfo_children():
         widgets.destroy()
+    frame.destroy()
 
 
 def help_callback():
@@ -55,58 +51,47 @@ def help_callback():
 
 def player_callback(frame):
     clear_frame(frame)
+    main_screen = Frame(window, width=1200, height=800)
+    main_screen.pack(side="top", expand=False, fill="both")
+
+    canvas = Canvas(main_screen, width=1200, height=800)
+    canvas.pack()
+
+    background = PhotoImage(file="assets/board2.png")
+    canvas.create_image(0, 0, image=background, anchor=NW)
+
+    # st-dr, sus-jos, img
+    # +70
+    utile = utils.Utils()
+    board = utile.init_board()
+    color_path = utile.color_coosing()
+    joc = game.Game(board, color_path, canvas)
+    joc.show_board()
+
+    window.mainloop()
 
 
 def pc_callback(frame):
     clear_frame(frame)
+    main_screen = Frame(window, width=1200, height=800)
+    main_screen.pack(side="top", expand=False, fill="both")
 
+    canvas = Canvas(main_screen, width=1200, height=800)
+    canvas.pack()
 
-def color_coosing():
-    a = random.randint(1, 9)
-    b = random.randint(1, 9)
-    while b == a or (a == 1 and b == 2) or (a == 2 and b == 3) or (a == 2 and b == 1) or (a == 3 and b == 2):
-        b = random.randint(1, 9)
+    background = PhotoImage(file="assets/board2.png")
+    canvas.create_image(0, 0, image=background, anchor=NW)
 
-    if a == 1:
-        path_a = "assets/blue.png"
-    elif a == 2:
-        path_a = "assets/light-blue.png"
-    elif a == 3:
-        path_a = "assets/grey.png"
-    elif a == 4:
-        path_a = "assets/black.png"
-    elif a == 5:
-        path_a = "assets/brown.png"
-    elif a == 6:
-        path_a = "assets/green.png"
-    elif a == 7:
-        path_a = "assets/purple.png"
-    elif a == 8:
-        path_a = "assets/red.png"
-    else:
-        path_a = "assets/white.png"
+    # st-dr, sus-jos, img
+    # +70
 
-    if b == 1:
-        path_b = "assets/blue.png"
-    elif b == 2:
-        path_b = "assets/light-blue.png"
-    elif b == 3:
-        path_b = "assets/grey.png"
-    elif b == 4:
-        path_b = "assets/black.png"
-    elif b == 5:
-        path_b = "assets/brown.png"
-    elif b == 6:
-        path_b = "assets/green.png"
-    elif b == 7:
-        path_b = "assets/purple.png"
-    elif b == 8:
-        path_b = "assets/red.png"
-    else:
-        path_b = "assets/white.png"
+    utile = utils.Utils()
+    board = utile.init_board()
+    color_path = utile.color_coosing()
+    joc = game.Game(board, color_path, canvas)
+    joc.show_board()
 
-    color_path = [path_a, path_b]
-    return color_path
+    window.mainloop()
 
 
 if __name__ == '__main__':
--- Table/game.py
+++ Table/game.py
@@ -8,32 +8,26 @@ class Game:
         self.board_1 = board[0]
         self.board_2 = board[1]
         self.color = color_path
+        self.image = [PhotoImage(file=self.color[0]), PhotoImage(file=self.color[1])]
         self.canvas = canvas
 
-    def show_board(self):
-        self.image_1 = PhotoImage(file=self.color[0])
-        for i in range(0, len(self.board_1)):  # nr de coloane
-            print(len(self.board_1[i][0]))
-            for j in range(0, len(self.board_1[i][0])):  # elementele din fiecare coloana
-                btn = Button(text="", image=self.image_1, relief=FLAT, borderwidth=0)
-                if i <= 11:
-                    button1_window = self.canvas.create_window(self.board_1[i][1], self.board_1[i][2] + 65 * j,
-                                                               window=btn)
-                elif i > 11:
-                    button1_window = self.canvas.create_window(self.board_1[i][1], self.board_1[i][2] - 65 * j,
-                                                               window=btn)
+    def show_player_board(self, id, color, board):
+        poz = [[65, -65], [-65, 65]]
 
-        self.image_2 = PhotoImage(file=self.color[1])
-        for i in range(0, len(self.board_2)):  # nr de coloane
-            print(len(self.board_2[i][0]))
-            for j in range(0, len(self.board_2[i][0])):  # elementele din fiecare coloana
-                btn = Button(text="", image=self.image_2, relief=FLAT, borderwidth=0)
+        for i in range(0, len(board)):  # nr de coloane
+            for j in range(0, len(board[i][0])):  # elementele din fiecare coloana
+                btn = Button(text="", image=self.image[id-1], relief=FLAT, borderwidth=0)
                 if i <= 11:
-                    button2_window = self.canvas.create_window(self.board_2[i][1], self.board_2[i][2] - 65 * j,
-                                                               window=btn)
+                    button_window = self.canvas.create_window(board[i][1], board[i][2] + poz[id - 1][0] * j,
+                                                              window=btn)
                 elif i > 11:
-                    button2_window = self.canvas.create_window(self.board_2[i][1], self.board_2[i][2] + 65 * j,
-                                                               window=btn)
+                    button_window = self.canvas.create_window(board[i][1], board[i][2] + poz[id - 1][1] * j,
+                                                              window=btn)
+
+    def show_board(self):
+        self.show_player_board(1, self.color[0], self.board_1)
+        self.show_player_board(2, self.color[1], self.board_2)
+
 
     def joc(self):
         turn = 1
